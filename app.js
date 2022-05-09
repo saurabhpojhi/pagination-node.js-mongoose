bc39d2dcc98f03f9b7cb80b878896d4b6037e412
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-10 02:50:32 +0530

    pagination done

--- app.js
+++ app.js
@@ -16,8 +16,26 @@ mongoose.connect('mongodb://localhost:27017/Pagination_New')
     console.log("Error:"+ error.Message)
 })
 
+//  creating Posts 
+app.post('/', async (req, res) => {
+    const post = new Posts(req.body);
+    await post.save();
+    res.status(200).send('Successfully created');
+});
 
-
+//  getting Post
+app.get('/', async (req, res) => {
+    try {
+        // Adding Pagination
+        const limitValue = req.query.limit || 5;
+        const skipValue = req.query.skip || 1;
+        const posts = await Posts.find()
+            .limit(limitValue).skip(skipValue);
+        res.status(200).send(posts);
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 // Starting the server
 app.listen((port),()=>{
